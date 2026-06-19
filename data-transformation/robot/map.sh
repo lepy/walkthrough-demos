199903b3c68a20e0e888e1a220e852b65a61d31c
joergwa <waitelonis@yovisto.com>
2026-06-19 08:40:56 +0200

    robot example

---
+++ data-transformation/robot/map.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# download robot utility
+if [ ! -f robot.jar ]; then
+    wget "https://github.com/ontodev/robot/releases/download/v1.9.10/robot.jar"
+fi
+
+# download recent pmdco
+if [ ! -f pmdco.owl ]; then
+    wget "https://w3id.org/pmd/co/" -o pmdco.owl
+fi
+
+
+java -jar robot.jar template --input pmdco.owl  --template template.tsv --output result.ttl
